a467ca9b59b09b522aabb497196387b89e29a87a
MasterMong <mongkon@outlook.com>
2023-01-18 10:30:36 +0000

    tostu

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+    // ทำหน้าเว็บให้เสร็จก่อน
+
+    $host = '';
+    $username = '';
+    $password = '';
+    $database = '';
+    $port = 3306;
+
+    // $mysql = mysqli_connect($host, $username, $password, $database, $port);
--- index.php
+++ index.php
@@ -1,13 +1,2 @@
-<!DOCTYPE html>
-<html lang="th">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PK Quota 65</title>
-    <link rel="stylesheet" href="assets/style.css">
-</head>
-<body>
-    Hello world
-</body>
-</html>
+<?php
+    include("task.html");
